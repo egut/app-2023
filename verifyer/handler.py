7e3e24d33d6d51d4ddb064b4ece70cf6b399da0f
Erik Günther <erik.gunther@gmail.com>
2023-01-31 20:31:42 +0100

    Added docstrings

--- verifyer/handler.py
+++ verifyer/handler.py
@@ -1,7 +1,16 @@
+"""
+Verify function for Mensa swag 2023
+"""
 import json
 
 
 def hello(event, context):
+    """
+    :param      event:    The event
+    :type       event:    { type_description }
+    :param      context:  The context
+    :type       context:  { type_description }
+    """
     body = {
         "message": "Go Serverless v3.0! Your function executed successfully!",
         "input": event,

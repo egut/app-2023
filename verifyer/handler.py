c6578cc3cf11ea48e0fac4c567fe051fa9134126
jimg01 <32138391+jimg01@users.noreply.github.com>
2023-03-28 20:36:36 +0200

    Så? lagt till Checkanswer i handler?

--- verifyer/handler.py
+++ verifyer/handler.py
@@ -1,21 +1,27 @@
 """
 Verify function for Mensa swag 2023
 """
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 14 20:19:37 2023
+@author: jimg
+"""
 import json
 
-
-def hello(event, context):
+def CheckAnswer(event, context):
     """
     :param      event:    The event
     :type       event:    { type_description }
     :param      context:  The context
     :type       context:  { type_description }
     """
-    body = {
-        "message": "Hejsan! Your function executed successfully!",
-        "input": event,
-    }
-
-    response = {"statusCode": 200, "body": json.dumps(body)}
-
+    #fiska ut medlemsnr och svar
+    svar = event.get("queryStringParameters")['svar'] #?
+    medlemsnr = event.get("queryStringParameters")['medlemsid'] #?
+    correctanswer = get_answer(medlemsnr)
+    if correctanswer == svar:
+        #Skicka id + tid till DB (Del av response?)
+        response = json.dumps({"message":"Correct"})
+    else:
+        response = json.dumps({"message":"Incorrect"})
     return response
